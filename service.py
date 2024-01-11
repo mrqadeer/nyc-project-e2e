20dcc9b6759881f05ac3af5129ef5d492e52b072
mrqadeer <mrqadeer1231122@gmail.com>
2024-01-11 13:21:44 +0500

    again6

--- service.py
+++ service.py
@@ -45,5 +45,5 @@ def predict(input_data: PredictionInput):
 
 if __name__ == "__main__":
     import uvicorn
-    # uvicorn.run(app, host="0.0.0.0", port=8080)
-    uvicorn.run(app, host="127.1.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
+    # uvicorn.run(app, host="127.1.0.0", port=8080)
